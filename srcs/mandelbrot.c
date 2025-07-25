62a441d61c7285545cc1671172b720871634289b
Niklas Weber <nweber@student.42Heilbronn.de>
2025-07-25 16:51:40 +0200

    switched back to original mandelbrot calc

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -6,31 +6,11 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 13:25:09 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/25 15:47:04 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/25 16:42:11 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-static int	mandelbrot_check_escape(double zr, double zi, int iter)
-{
-	double	magnitude;
-
-	magnitude = zr * zr + zi * zi;
-	if (magnitude > 4.0)
-		return (1);
-	if (iter > 16 && magnitude < 0.001)
-		return (2);
-	return (0);
-}
-
-static void	mandelbrot_update_z(double *zr, double *zi, double *zr2, double *zi2, double cr, double ci)
-{
-	*zi = 2 * (*zr) * (*zi) + ci;
-	*zr = (*zr2) - (*zi2) + cr;
-	*zr2 = (*zr) * (*zr);
-	*zi2 = (*zi) * (*zi);
-}
 
 int	mandelbrot(double cr, double ci)
 {
@@ -39,22 +19,21 @@ int	mandelbrot(double cr, double ci)
 	double	zr2;
 	double	zi2;
 	int		iter;
-	int		escape_check;
 
 	zr = 0.0;
 	zi = 0.0;
 	zr2 = 0.0;
 	zi2 = 0.0;
 	iter = 0;
-	while (iter < MAX_ITER)
+	while (zr2 + zi2 <= 4.0 && iter < MAX_ITER)
 	{
-		escape_check = mandelbrot_check_escape(zr, zi, iter);
-		if (escape_check == 1)
-			break ;
-		if (escape_check == 2)
-			return (MAX_ITER);
-		mandelbrot_update_z(&zr, &zi, &zr2, &zi2, cr, ci);
+		zi = 2 * zr * zi + ci;
+		zr = zr2 - zi2 + cr;
+		zr2 = zr * zr;
+		zi2 = zi * zi;
 		iter++;
+		if (iter > 16 && zr2 + zi2 < 0.001)
+			return (MAX_ITER);
 	}
 	return (iter);
 }
